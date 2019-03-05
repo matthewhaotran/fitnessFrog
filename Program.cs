6464e60e862cee777d03bc80148856bb207b00c2
matthewhaotran@gmail.com <matthewhaotran@gmail.com>
2019-03-04 22:51:33 -0800

    initial commit

---
+++ Program.cs
@@ -0,0 +1,17 @@
+﻿namespace treehouse {
+        class Program
+    {
+        static void Main()
+        {
+            // Prompt user for minutes exercised 
+            System.Console.Write("Enter how many minutes you exercised: ");
+
+            string entry = System.Console.ReadLine();
+
+            // Add minutes exercised to total 
+            // Display total minutes exercised to the screen 
+            System.Console.WriteLine("You've exercised for " + entry + " minutes.");
+            // Repeat until user quits    
+        }
+    }
+}
